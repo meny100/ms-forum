94b0f732d7f62068d5413a28039212516fa312cd
Menachem Saada <menisaada@gmail.com>
2021-11-02 14:34:51 +0200

    Delete ms_forum/client/src directory

--- ms_forum/client/src/components/common/pageHeader.jsx
+++
@@ -1,9 +0,0 @@
-const PageHeader = ({ titleText }) =>
-    
-    <div className="row">
-        <div className="col-12 mt-4 font-italic">
-            <h1>{titleText}</h1>
-        </div>
-    </div>
-
-export default PageHeader;
--- ms_forum/client/src/components/footer.jsx
+++
@@ -1,4 +0,0 @@
-const Footer = () => <p className="border-top pt-3 text-center">
-    MS-FORUM © {new Date().getFullYear()}
-</p>
-export default Footer;
